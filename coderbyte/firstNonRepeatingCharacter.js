a46638551a50fa9ffb2092b51557b4d4e6189a9f
Anna-12-Mer <mammeramna@gmail.Com>
2021-02-02 14:46:25 +0100

    coderbyte

---
+++ coderbyte/firstNonRepeatingCharacter.js
@@ -0,0 +1,10 @@
+function firstNonRepeatingCharacter (str){
+        //   aaabcccdeeef
+        //  return 'b';
+    for(let i = 0 ; i<str.length; i++){
+        let char = str[i]; 
+        if(str.indexOf(char)== i && str.indexOf(char, i+1))
+            return char; 
+    }
+    return '_'; 
+}
