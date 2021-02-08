96f72ec333eb42b8979ff91294d8a9fb3183c157
Anna-12-Mer <mammeramna@gmail.Com>
2021-02-08 10:38:52 +0100

    leetcode

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -16,3 +16,31 @@ var twoSum = (nums, target)=>{
     }
     return result; 
 }; 
+
+//  other method 
+
+var twoSum_secondeMethod = (nums, target)=>{
+    const map = new Map();
+    for(let i = 0 ; i< nums.length; i++){
+        let curentVal= nums[i]; 
+        if(map.has(curentVal)){
+            return [map.get(curentVal), i]; 
+        }
+        let diff = target - nums[i]; 
+        map.set(diff,i);
+    }
+};
+
+//  using code Vanila JavaScript 
+
+var twoNums= (nums, target)=>{
+    let result ; 
+    nums.forEach((e,index) => {
+        for(let i = index +1 ; i< nums.length; i++){
+            if(e + nums[i]=== target){
+                return  result = [nums.indexOf(e), i]; 
+            }
+        }
+    });
+    return result; 
+}
